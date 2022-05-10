1d0b037374a72dd1ed071c3b237acc54bb537010
Uiskam <92760953+Uiskam@users.noreply.github.com>
2022-05-10 22:31:31 +0200

    lap szyba

--- src/main/java/GUI/Cell.java
+++ src/main/java/GUI/Cell.java
@@ -93,12 +93,45 @@ public class Cell extends StackPane {
     public void setTemperature(double newTemp) { this.temperature = newTemp;}
 
     private Color getColor() {
-        Color warmestColor = Color.rgb(255,13,13);
+        /*Color warmestColor = Color.rgb(255,13,13);
         Color coldestColor = Color.rgb(0,43,142);
-        double percent = 0.5;
+        double percent = this.getTemperature() / 100;
         int resultRed = (int) (coldestColor.getRed() + percent * (warmestColor.getRed() - coldestColor.getRed()));
         int resultGreen = (int) (coldestColor.getGreen() + percent * (warmestColor.getGreen() - coldestColor.getGreen()));
         int resultBlue = (int) (coldestColor.getBlue() + percent * (warmestColor.getBlue() - coldestColor.getBlue()));
+        System.out.println(coldestColor.getRed() + " " + warmestColor.getRed() + " " + resultBlue);*/
+        double curTemp = this.getTemperature();
+        if(curTemp >= 90) {
+            return Color.rgb(255,13,13);
+        }
+        if(80 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(70 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(60 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(50 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(40 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(30 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(20 <= curTemp) {
+            return Color.rgb(255,13,13);
+        }
+        if(10 <= curTemp) {
+            return Color.rgb(255, 13, 13);
+        }
+        else {
+            return Color.rgb(255,13,13);
+        }
+
         return Color.rgb(resultRed, resultGreen, resultBlue);
     }
 }
