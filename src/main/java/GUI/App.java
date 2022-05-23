689486403e12bdf8fd68edd3df53fd55c8e4068f
bartn1k <bartek.slupik@gmail.com>
2022-05-23 20:23:04 +0200

    Updated task scheduling and flushing after exit

--- src/main/java/GUI/App.java
+++ src/main/java/GUI/App.java
@@ -1,22 +1,49 @@
 package GUI;
 
 import Models.HeatScenario;
-import Models.ICellType;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class App extends Application {
     private Stage primaryStage;
     private DisplayGrid displayGrid;
-    private ComboBox<ICellType> typeSelectBox;
-    private int simulationUpdatePause, GUIUpdatePause;
+    private final Timer GUITimer = new Timer();
+    private int simulationUpdatePause;
+    private Thread simulationThread;
+
+    class GUIUpdate extends TimerTask {
+        @Override
+        public void run() {
+            Platform.runLater(() -> displayGrid.updateAll());
+        }
+    }
+
+    private void simulation() {
+        while (true) {
+            try {
+                //noinspection BusyWait
+                Thread.sleep(simulationUpdatePause);
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+            this.displayGrid.gridStack.tickAll();
+        }
+    }
+
+    @Override
+    public void stop() {
+        simulationThread.interrupt();
+        Platform.exit();
+        System.exit(0);
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -27,10 +54,10 @@ public class App extends Application {
         this.buildGrid();
         this.createGUI();
 
-        Thread guiUpdateThread = new Thread(this::updateGui);
-        guiUpdateThread.start();
+        int GUIUpdatePause = 15;
+        GUITimer.schedule(new GUIUpdate(), 0, GUIUpdatePause);
 
-        Thread simulationThread = new Thread(this::simulation);
+        simulationThread = new Thread(this::simulation);
         simulationThread.start();
     }
 
@@ -41,17 +68,6 @@ public class App extends Application {
 
     private void initialize() {
         this.simulationUpdatePause = 100;
-        this.GUIUpdatePause = 15;
-    }
-
-    private void simulation() {
-        while (true) {
-            try {
-                Thread.sleep(simulationUpdatePause);
-            } catch (InterruptedException ignore) {}
-
-            this.displayGrid.gridStack.tickAll();
-        }
     }
 
     private void createGUI() {
@@ -81,15 +97,5 @@ public class App extends Application {
         Scene scene = new Scene(sceneVBox);
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
-    }
-
-    private void updateGui() {
-        while (true) {
-            try {
-                Thread.sleep(GUIUpdatePause);
-            } catch (InterruptedException ignore) {}
-
-            Platform.runLater(() -> this.displayGrid.updateAll());
-        }
     }
 }
